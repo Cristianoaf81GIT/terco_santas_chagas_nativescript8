9eb34b1bc7efa7b43f4a353569efc3a780d4263b
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-11-12 14:24:03 -0300

    add bottom-sheet

--- src/app/monday/monday.component.ts
+++ src/app/monday/monday.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core'
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { Application, StackLayout, CoreTypes } from '@nativescript/core';
 
 @Component({
   selector: 'Browse',
@@ -8,16 +8,33 @@ import { Application } from '@nativescript/core'
   styleUrls: ['monday.scss']
 })
 export class MondayComponent implements OnInit {
+
+  @ViewChild('bottomSheet')
+  private bottomSheet!: any;
+
+  backgroundColor = '#fff';
+
   constructor() {
-    // Use the component constructor to inject providers.
   }
 
   ngOnInit(): void {
-    // Init your component properties here.
   }
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
+  }
+
+  onTap() {
+    if (this.bottomSheet !== null) {
+      const elm = this.bottomSheet.nativeElement as StackLayout;
+      const { height } = elm.getActualSize();
+      this.backgroundColor = height <=0 ? 'rgba(40,40,40,0.75)' : '#fff';
+      elm.animate({
+        height: height <= 0 ? '100%' : '0%',
+        curve: CoreTypes.AnimationCurve.cubicBezier(.18,.43,.78,.49),
+        duration: 1
+      });
+    }
   }
 }
